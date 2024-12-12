7d813932ef6b13a84ae3295b01c4c0c56f643d95
dev.zakariaelkashef <dev.zakariaelkashef@gmail.com>
2024-12-12 10:53:16 +0200

    add most Saled Product to section

--- app/Http/Controllers/Booth/Api/SectionController.php
+++ app/Http/Controllers/Booth/Api/SectionController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers\Booth\Api;
 
 use App\Models\Store;
 use App\Models\Slider;
+use App\Models\Product;
 use App\Models\Section;
+use App\Models\OrderItem;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\Booth\ProductInHomeResource;
 use App\Http\Resources\Booth\StoreRecource;
 use App\Http\Resources\Booth\SliderResource;
 use App\Http\Resources\Booth\SectionResource;
-use App\Models\Product;
+use App\Http\Resources\Booth\ProductInHomeResource;
 
 class SectionController extends Controller
 {
@@ -27,12 +28,17 @@ class SectionController extends Controller
 
     public function show(Section $section)
     {
+        $mostSaledProductIds = OrderItem::mostSoldByApp('booth')->pluck('product_id')->toArray();
+        $mostSaledProducts = Product::whereIn('id', $mostSaledProductIds)->whereHas('store', function($store) use($section) {
+            $store->where('section_id', $section->id);
+        })->get();
+
         $data = [
             'ad' => '',
             'sliders' => SliderResource::collection(Slider::booth()->active()->get()),
             'stores' => StoreRecource::collection($section->stores()->active()->get()),
             'most_recent' => ProductInHomeResource::collection(Product::inSection($section->id)->active()->take(6)->get()),
-            'most_sale' => []
+            'most_sale' => ProductInHomeResource::collection($mostSaledProducts),
         ];
 
         return $this->sendResponse(200, $data);
