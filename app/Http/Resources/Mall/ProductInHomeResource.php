f07e0edad73744542ed03b9b98b0f0d620d9253b
DevZakariaElkashef <dev.zakariaelkashef@gmail.com>
2024-12-24 22:53:59 +0200

    add prod image and store id to mall

--- app/Http/Resources/Mall/ProductInHomeResource.php
+++ app/Http/Resources/Mall/ProductInHomeResource.php
@@ -14,13 +14,15 @@ class ProductInHomeResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        
+
         return [
             'id' => $this->id,
             'name' => $this->name ?? '',
             'description' => $this->description ?? '',
-            'store_id' => $this->store->name ?? '',
+            'product_image' => $this->image ? asset($this->image)  : '',
+            'store_id' => $this->store->id ?? 0,
             'store_name' => $this->store->name ?? '',
+            'store_image' => $this->store && $this->store->image ? asset($this->store->image)  : '',
         ];
     }
 }
