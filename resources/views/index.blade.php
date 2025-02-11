96e06fc379eb53db87def165660d9a3a8ba3eae9
mustafashokry <mustafashokry555@gmail.com>
2025-02-11 09:09:28 +0200

    push

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -105,7 +105,7 @@
             <div class="card overflow-hidden sales-card bg-success-gradient">
                 <div class="pl-3 pt-3 pr-3 pb-2">
                     <div class="">
-                        <h6 class="mb-3 tx-12 text-white">{{ __('main.stores') }}</h6>
+                        <h6 class="mb-3 tx-12 text-white">{{ __('main.mall') }}</h6>
                     </div>
                     <div class="pb-0 mt-0">
                         <div class="d-flex">
@@ -241,7 +241,7 @@
         <div class="card overflow-hidden sales-card bg-success-gradient">
             <div class="pl-3 pt-3 pr-3 pb-2">
                 <div class="">
-                    <h6 class="mb-3 tx-12 text-white">{{ __('main.stores') }}</h6>
+                    <h6 class="mb-3 tx-12 text-white">{{ __('main.booth') }}</h6>
                 </div>
                 <div class="pb-0 mt-0">
                     <div class="d-flex">
