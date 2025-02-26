c1bb3c996f4fa3ec0915adc20999b0948add67ce
mustafashokry555 <mustafashokry555@gmail.com>
2025-02-26 09:59:27 +0200

    app to the CommissionTransactionController

--- app/Http/Controllers/Api/CommissionTransactionController.php
+++ app/Http/Controllers/Api/CommissionTransactionController.php
@@ -6,41 +6,42 @@ use App\Http\Controllers\Controller;
 use App\Models\AppSetting;
 use App\Models\CommissionTransaction;
 use App\Models\Product;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 
 class CommissionTransactionController extends Controller
 {
+    use ApiResponse;
     public function store(Request $request)
     {
 
         try {
             $request->validate([
-                'transaction_id' => 'required',
-                'user_id' => 'required|exists:users,id',
-                'product_id' => 'required|exists:products,id',
-                'time' => 'required|date',
+                'transaction_id' => 'required|string',
+                // 'user_id' => 'required|exists:users,id',
+                // 'product_id' => 'required|exists:products,id',
+                'time' => 'required',
                 'total_amount' => 'nullable|numeric',
+                'app' => 'required|in:rfoof,resale',
             ]);
         }  catch (ValidationException $e) {
             $errorMessage = $e->validator->errors()->first();
             return $this->sendResponse(400, '', $errorMessage);
         }
-        $app = Product::find($request->product_id)->app;
+        $app = $request->app;
         $commission_percentage = AppSetting::where('key', "commission_$app")->first();
         if($commission_percentage){
-            $commission_percentage = $commission_percentage->pluck('value_ar');
+            $commission_percentage = $commission_percentage->value_ar;
         }else{
             $commission_percentage = 0;
         }
-
         $commissionTransaction = CommissionTransaction::create([
             'transaction_id' => $request->transaction_id,
-            'user_id' => $request->user_id,
-            'product_id' => $request->product_id,
+            'user_id' => $request->user()->id,
             'time' => $request->time,
             'total_amount' => $request->total_amount,
-            'commission_percentage' => $commission_percentage,
+            'commission_percentage' => ($commission_percentage),
             'app' => $app,
         ]);
         // return response()->json(['message' => 'Commission transaction created successfully', 'data' => $commissionTransaction], 201);
