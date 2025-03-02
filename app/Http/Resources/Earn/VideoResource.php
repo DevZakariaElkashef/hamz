b5990922cc8e6fbfddb38edbacf69fc78e9ced38
Mustafa Shokry <mustafashokry555@gmail.com>
2025-03-02 12:38:46 +0200

    video resoure

--- app/Http/Resources/Earn/VideoResource.php
+++ app/Http/Resources/Earn/VideoResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Earn;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -22,18 +23,19 @@ class VideoResource extends JsonResource
             "thumbnail"=> $this->thumbnail,
             "category_id"=> $this->category_id,
             "store_url"=> $this->store_url ,
-            "store"=> [
+            "store"=> $this->store_id ? [
                 'id' => $this->store->id ?? '',
                 'name' => $this->store->name ?? '',
                 'image' => $this->store ? asset($this->store->image) : '',
                 'app' => $this->store->app ?? '',
-            ],
+            ] : null,
             "reword_amount"=> $this->reword_amount,
             "is_active"=> $this->is_active,
             "duration"=> $this->duration,
             "app"=> $this->app,
             "created_at"=> $this->created_at,
-            "updated_at"=> $this->updated_at
+            'created_at' => Carbon::parse($this->created_at)->format('Y-m-d h:i:s'),
+            'updated_at' => Carbon::parse($this->updated_at)->format('Y-m-d h:i:s'),
         ];
     }
 }
